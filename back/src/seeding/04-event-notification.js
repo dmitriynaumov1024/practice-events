70c6c2007c2eb3426625ee7da8000c4fb3c1d8f3
Dmitriy Naumov <naumov1024@gmail.com>
2023-12-04 14:32:43 +0200

    back: seeding: seed all entity types

--- back/src/seeding/04-event-notification.js
+++ back/src/seeding/04-event-notification.js
@@ -1,5 +1,32 @@
+import { Random } from "./utils.js"
+
+let notifyTimeouts = [ 
+    60000, 600000, 3600000 
+]
+
+let notifyIntervals = [
+    60000, 600000
+]
+
 async function seedEventNotification (db) {
     console.log("[i]: Seeding EventNotification")
+    let visits = await db.eventVisit.query().withGraphJoined("event").limit(1000)
+    let notifications = [ ]
+    for (let visit of visits) {
+        if (Random.probability(0.8)) {
+            notifications.push({
+                personId: visit.personId,
+                eventId: visit.eventId,
+                createadAt: visit.createadAt,
+                notifyAt: new Date(visit.event.startsAt.valueOf() - Random.choice(notifyTimeouts)),
+                interval: Random.choice(notifyIntervals),
+                attempts: Random.integer(1, 3)
+            })
+        }
+    }
+    for (let notification of notifications) {
+        await db.eventNotification.query().insert(notification)
+    }
 }
 
 export {
--- back/src/seeding/utils.js
+++ back/src/seeding/utils.js
@@ -1,17 +1,5 @@
 class Random 
 {
-    static integer (min, max) {
-        if (min == undefined) {
-            min = 0
-            max = 32767
-        }
-        if (max == undefined) {
-            max = min
-            min = 0
-        }
-        return ((Math.random() * (max - min)) | 0) - min
-    }
-
     static float (min, max) {
         if (min == undefined) {
             max = 32767
@@ -20,11 +8,24 @@ class Random
             max = min
             min = 0
         }
-        return (Math.random() * (max - min)) - min
+        return Math.random() * (max - min) + min
+    }
+
+    static integer (min, max) {
+        return Math.floor(this.float(min, max))
     }
 
     static choice (items) {
-        return items[this.integer(items.length-1)]
+        return items[this.integer(0, items.length)]
+    }
+
+    static array (minSize, maxSize, generatorFunc) {
+        let size = this.integer(minSize, maxSize)
+        return Array(size).fill(0).map(generatorFunc)
+    }
+
+    static probability (p) {
+        return p > Math.random()
     }
 }
 
