bf02992da7edf46f5095c15c6382a5cd5ea02bca
Dmitriy Naumov <naumov1024@gmail.com>
2023-12-08 12:27:11 +0200

    back: webapi: person: handle person tags, hide password

--- back/src/webapi/route/person.js
+++ back/src/webapi/route/person.js
@@ -7,6 +7,7 @@ async function view (request, response) {
     let { user, db, logger, query } = request
     let id = Number(query.id) || -1
     let person = await db.person.query()
+        .withGraphJoined("tags")
         .where("id", id)
         .first()
 
@@ -17,19 +18,23 @@ async function view (request, response) {
             message: "Not found"
         })
     }
-    if (!person.isPublic && person.id != user.id) {
-        return response.status(404).json({
-            success: false,
-            unauthorized: true,
-            message: "Not found"
+    if (person.isPublic || person.id == user.id) {
+        person.password = null
+        return response.status(200).json({ 
+            person: person, 
+            success: true
         })
     }
 
-    return response.status(200).json({ 
-        person: person, 
-        success: true, 
-        message: "Not implemented yet" 
+    return response.status(200).json({
+        person: {
+            id: person.id,
+            email: person.email
+        },
+        success: true,
+        message: "Not public"
     })
+    
 }
 
 async function list (request, response) {
@@ -37,10 +42,18 @@ async function list (request, response) {
     let page = Number(request.query.page) || 1
 
     let persons = db.person.query()
-        .where("isPublic", true)
         .orderBy("id", "desc")
 
     let items = await paginate(page, pageSize, ()=> persons)
+    items = items.map(item => {
+        item.password = null
+        if (item.isPublic) return item
+        else return {
+            id: item.id,
+            email: item.email,
+            createdAt: item.createdAt
+        }
+    })
 
     return response.status(200).json(items)
 }
@@ -58,6 +71,7 @@ async function create (request, response) {
     }
     try {
         person = await db.person.query().insert(person)
+        person.password = null
         return response.status(200).json({
             person: person,
             success: true
@@ -73,10 +87,18 @@ async function create (request, response) {
     }
 }
 
+function diffTags (oldTags, newTags) {
+    oldTags = new Set(oldTags.map(tag => tag.tag ?? tag))
+    newTags = new Set(newTags.map(tag => tag.tag ?? tag))
+    return {
+        create: [...newTags].filter(tag => !oldTags.has(tag)),
+        delete: [...oldTags].filter(tag => !newTags.has(tag))
+    }
+}
+
 async function update (request, response) {
     let { user, db, logger } = request
     let id = Number(request.query.id)
-    let input = request.body.person
     if (id != user.personId) {
         return response.status(401).json({
             unauthorized: true,
@@ -85,19 +107,35 @@ async function update (request, response) {
         })
     }
 
+    let input = request.body.person
+    input.tags = input.tags.map(item => ({
+        personId: item.personId ?? id,
+        tag: (item.tag ?? item).toLowerCase()
+    }))
+
     let person = await db.person.query()
+        .withGraphJoined("tags")
         .where("id", id)
         .first()
-
+    
+    let diff = diffTags(person.tags, input.tags)
+    
     person.name = input.name
     person.isPublic = !!input.isPublic
     person.biography = input.biography || ""
-    person.password = hash(input.password)
+    if (input.password) person.password = hash(input.password)
+    person.tags = input.tags
 
     try {
         await db.person.query()
             .where("id", id)
             .update(person)
+        await db.personTag.query().delete()
+            .where("personId", person.id)
+            .whereIn("tag", diff.delete)
+        await db.personTag.query()
+            .insertGraph(diff.create.map(tag => ({ personId: person.id, tag: tag })))
+        person.password = null
         return response.status(200).json({
             person: person,
             success: true
