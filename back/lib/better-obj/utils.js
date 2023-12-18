aefb47b8356c6dcf8c33c653651c8f767bc8e32c
Dmitriy Naumov <naumov1024@gmail.com>
2023-12-18 12:03:34 +0200

    implement sending email with node-mailjet

--- back/lib/better-obj/utils.js
+++ back/lib/better-obj/utils.js
@@ -37,6 +37,30 @@ function offsetDate (...args) {
     }
 }
 
+function diffDate (date1, date2) {
+    // not like difference of 2 numbers, but more like vector length
+    // how much ms we have to spend to reach date 2 from date 1
+    if (! date1 instanceof Date) date1 = new Date(date1)
+    if (! date2 instanceof Date) date2 = new Date(date2)
+    return date2.valueOf() - date1.valueOf()
+}
+
+function formatDiffDate (diff) {
+    let s = Math.floor(Math.abs(diff / 1000))
+    let m = Math.floor(s / 60)
+    let h = Math.floor(m / 60)
+    let d = Math.floor(h / 24)
+    let interval = `${d} days, ${h%24} hours, ${m%60} minutes`
+    if (diff < 0) return interval + " ago"
+    else return "in " + interval
+}
+
+function formatDateTime(date) {
+    if (!(date instanceof Date)) date = new Date(date)
+    return `${date.getUTCFullYear()}-${String(date.getUTCMonth()+1).padStart(2, "0")}-${String(date.getUTCDate()).padStart(2, "0")} `
+         + `${String(date.getUTCHours()).padStart(2, "0")}:${String(date.getUTCMinutes()).padStart(2, "0")}`
+}
+
 function ymdToDate (year, month, day) {
     year = String(year).padStart(4, "0")
     month = String(month).padStart(2, "0")
@@ -60,6 +84,9 @@ export {
     hash,
     randomHash,
     offsetDate,
+    diffDate,
+    formatDiffDate,
+    formatDateTime,
     ymdToDate,
     inRange,
     clamp
--- back/src/workers/send-email.js
+++ back/src/workers/send-email.js
@@ -2,7 +2,50 @@ import "dotenv/config"
 import { Worker } from "./worker.js"
 import { ConsoleLogger } from "logging"
 import { EventsDbAdapter, Knex } from "database"
-import { offsetDate } from "better-obj"
+import { offsetDate, formatDateTime, diffDate, formatDiffDate } from "better-obj"
+
+import Mailjet from "node-mailjet"
+
+function createMailStub() {
+    return {
+        get type() {
+            return "Stub"
+        },
+        async send (message) {
+            return { message: "This stub works." }
+        }
+    }
+}
+
+function createMailjet() {
+    let mailer = Mailjet.apiConnect (
+        process.env.MAILJET_PUBLIC_KEY, 
+        process.env.MAILJET_SECRET_KEY
+    )
+    return {
+        get type() {
+            return "Mailjet"
+        },
+        async send (message) {
+            return await mailer
+                .post("send", { version: "v3.1" })
+                .request({ Messages: [ message ] })
+        }
+    }
+}
+
+const mailProviders = {
+    "mailjet": createMailjet,
+    [undefined]: createMailStub 
+}
+
+const knownMailTypes = {
+    "mailjet": "mailjet"
+}
+
+function createEmailAdapter() {
+    return mailProviders[knownMailTypes[process.env.MAIL_TYPE?.toLowerCase()]]()
+}
 
 class SendEmailWorker extends Worker
 {
@@ -13,31 +56,48 @@ class SendEmailWorker extends Worker
     async init() {
         this.logger = new ConsoleLogger()
         this.db = new EventsDbAdapter()
+        this.mail = createEmailAdapter()
+        this.senderEmail = process.env.MAIL_SENDER 
         await this.db.connect(Knex.parseEnv())
     }
 
     async action() {
         let notifications = await this.db.eventNotification.query()
-            .joinRelated("event")
-            .joinRelated("person")
+            .withGraphJoined("[event, person]")
             .where("attempts", ">", 0)
             .where("notifyAt", "<", new Date())
             .limit(10)
 
         for (let notification of notifications) {
+            await this.handleNotification(notification)
             notification.notifyAt = offsetDate(notification.notifyAt, notification.interval)
             notification.attempts -= 1
             await this.db.eventNotification.query()
                 .where("eventId", notification.eventId)
                 .where("personId", notification.personId)
                 .update(notification)
-            await this.handleNotification(notification)
         }
     }
 
     async handleNotification (notification) {
-        // this is definitely a stub.
         this.logger.log(`Notifying user #${notification.personId} about event #${notification.eventId}`)
+        try {
+            let result = await this.mail.send({
+                From: {
+                    Email: this.senderEmail
+                },
+                To: [{
+                    Email: notification.person.email
+                }],
+                Subject: "Event reminder",
+                TextPart: `Hello there, ${notification.person.name}, we remind you that event '${notification.event.title}' `
+                        + `is starting on ${formatDateTime(notification.event.startsAt)} UTC `
+                        + `(${formatDiffDate(diffDate(new Date(), notification.event.startsAt))})`
+            })
+        }
+        catch (error) {
+            this.logger.error(error.stack)
+        }
     }
 }
 
